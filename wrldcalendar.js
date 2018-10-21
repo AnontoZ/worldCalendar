d72a8457fb198d32efaa49d53ba023806b6fca58
Rowe-AndersonHu1 <42788866+Rowe-AndersonHu1@users.noreply.github.com>
2018-10-21 01:11:12 -0400

    Add files via upload

--- wrldcalendar.js
+++ wrldcalendar.js
@@ -1,23 +1,26 @@
-var months = [ "January","February","March","April","May","June","July","August","September",
-	"October","November","December"]
+"use strict";
+
+
+var months = [ "January","February","March","April","May","June","July","August","September","October","November","December"]; 
 
 function dispDays(numDays, currDay) {
-	text = "";
-	for (i = 1; i < numDays; i++) {
+	document.getElementById("calDays").innerHTML = "";
+	var text = "";
+	for ( var i = 1; i < numDays; i++) {
 	  if (i == currDay) {
-	  	text += "<li><span class='active'>" + i.toString() + "</span></li>"
+	  	text += "<li><span class='active'>" + i.toString() + "<br></span></li>"
 	  } 
 	  else {
-	  	text += "<li>"+ i.toString() + "</li>";
+	  	text += "<li>"+ i.toString() + "<br></li>";
 	  }
 	}
 	document.getElementById("calDays").innerHTML = text;
 }
 
 function nextMonth() {
-	month = months.indexOf(document.getElementById("currMonth").textContent);
-	month = month+1;
-	year = parseInt(String(document.getElementById("currYear").textContent));
+	var month = months.indexOf(document.getElementById("currMonth").textContent);
+	var month = month+1;
+	var year = parseInt(String(document.getElementById("currYear").textContent));
 	if (month > 11) {
 		month -= 11;
 		year = year + 1;
@@ -27,9 +30,9 @@ function nextMonth() {
 }
 
 function prevMonth() {
-	month = months.indexOf(document.getElementById("currMonth").textContent);
-	month = month-1;
-	year = parseInt(String(document.getElementById("currYear").textContent));
+	var month = months.indexOf(document.getElementById("currMonth").textContent);
+	var month = month-1;
+	var year = parseInt(String(document.getElementById("currYear").textContent));
 	if (month < 0) {
 		month += 11;
 		year = year - 1;
